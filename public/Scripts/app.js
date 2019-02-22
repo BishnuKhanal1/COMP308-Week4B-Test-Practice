0a248660f0f173ca0c4fd513d09d19f27026ec93
Mr. Bishnu Prasad Khanal <bkhanal1@my.centennialcollege.ca>
2019-02-21 22:27:41 -0500

    Initial Commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,13 @@
+/*--Custom JavaScripts */
+//IIFE-- Immediately invoked function expression
+
+(function(){
+
+   function Start(){
+    console.log(`%c App Started`, 
+    "font-size:20px; color:blue;font-weight:bold;")
+
+   } 
+
+   window.addEventListener("load", Start);
+})();
